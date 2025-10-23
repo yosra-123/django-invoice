39c626ede1b95d45ef295d6f18f991c6175134bc
Yosra BK <76393807+yosra-123@users.noreply.github.com>
2025-10-23 14:56:32 +0100

    Update settings.py

--- django_invoice/settings.py
+++ django_invoice/settings.py
@@ -1,51 +1,73 @@
-
-
+import os
 from pathlib import Path
-import os 
+from decouple import config
+from django.utils.translation import gettext_lazy as _
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# ------------------------------
+# BASE DIRECTORIES
+# ------------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+# ------------------------------
+# SECURITY
+# ------------------------------
+SECRET_KEY = config('SECRET_KEY', default="django-invoiceadfasdfa")
+ALLOWED_HOSTS = ['django-invoice.onrender.com']
 
-LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale'),
-)
+# ------------------------------
+# STATIC & MEDIA FILES
+# ------------------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-try:
-    from django.contrib.messages import constants as messages
-    MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-info',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger'
-    }
-except Exception as e:
-    pass    
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-SECRET_KEY = 'django-insecure-s#6o)r^rao3oedk&fb__@=j$ol6twp5)rtuvoj8#c(-i(c%hpm'
+# ------------------------------
+# TEMPLATES & LOCALE
+# ------------------------------
+TEMPLATES_DIR = BASE_DIR / 'templates'
+LOCALE_PATHS = [BASE_DIR / 'locale']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# ------------------------------
+# DJANGO MESSAGE TAGS
+# ------------------------------
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
-
+# ------------------------------
+# APPLICATIONS
+# ------------------------------
 INSTALLED_APPS = [
+    # Django default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'whitenoise.runserver_nostatic',
+    'django_celery_beat',
+
+    # Local apps
     'fact_app',
 ]
 
+# ------------------------------
+# MIDDLEWARE
+# ------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,10 +75,23 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+# ------------------------------
+# URL & WSGI CONFIG
+# ------------------------------
 ROOT_URLCONF = 'django_invoice.urls'
+WSGI_APPLICATION = 'django_invoice.wsgi.application'
 
+# ------------------------------
+# STATIC FILES STORAGE
+# ------------------------------
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# ------------------------------
+# TEMPLATES CONFIG
+# ------------------------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -73,75 +108,35 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_invoice.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
+# ------------------------------
+# PASSWORD VALIDATORS
+# ------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
+# ------------------------------
+# INTERNATIONALIZATION
+# ------------------------------
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-from django.utils.translation import gettext_lazy as _
-
 LANGUAGES = [
-('fr', _('French')),
-('en', _('English')),
+    ('fr', _('French')),
+    ('en', _('English')),
 ]
 
-USE_I18N = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+# ------------------------------
+# DEFAULT PRIMARY KEY FIELD
+# ------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# ------------------------------
+# LOGIN
+# ------------------------------
 LOGIN_URL = 'admin:login'
